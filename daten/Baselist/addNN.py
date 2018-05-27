9f709698e810bd66d750c5726af9fcb6177e6566
Duke <dueker@cl.uni-heidelberg.de>
2018-05-27 15:04:13 +0200

    Duplikate zwischen SentiWS und Rap-Beleidigungen aus Baselist entfernt und entsprechend Wörter nachgereicht

--- daten/Baselist/addNN.py
+++ daten/Baselist/addNN.py
@@ -2,6 +2,16 @@
 with open('insults_handsortiert.txt', 'r') as read_file:
     insults_list = read_file.read().splitlines()
 
-with open('insults_handsortiert_tagged.txt', 'w+') as write_file:
-    for insult in insults_list:
-        write_file.write(insult+'|NN\n')
+with open('insults_handsortiert_tagged.txt', 'r') as read_file:
+    insults_list_t = read_file.read().splitlines()
+
+with open('SentiWS_negativ_sorted.txt', 'r') as read_file:
+    senti_list = read_file.read().splitlines()
+
+for word in insults_list_t:
+    if(word in senti_list):
+        print(word)
+
+#with open('insults_handsortiert_tagged.txt', 'w+') as write_file:
+#    for insult in insults_list:
+#        write_file.write(insult+'|NN\n')
