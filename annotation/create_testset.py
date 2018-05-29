7eb8ccb4d330a78c343adbb35c4c566c61a4b4d8
Ulf2016 <steinbach@cl.uni-heidelberg.de>
2018-05-29 18:16:52 +0200

    Annotations Testset (50N, 50V, 50A) + Skript für autom. Testset Generierung hinzugefügt

---
+++ annotation/create_testset.py
@@ -0,0 +1,38 @@
+from random import randrange
+from random import seed
+import sys
+
+def create_annotation_testset(path_to_baselist, n=50):
+	"""
+	Inputfile: Baselist 
+	"""
+	seed(42)
+	nouns = []
+	verbs = []
+	adj = []
+	final_list = []
+	out = open('./annotation_testset.txt', 'w')
+	with open(path_to_baselist, 'r', encoding="utf-8") as inputfile:
+		for line in inputfile:
+			token = line.split("|")[0].strip()
+			tag = line.split("|")[1].strip()
+			if tag == "NN":
+				nouns.append(line)
+			elif tag == "VVINF":
+				verbs.append(line)
+			elif tag == "ADJX":
+				adj.append(line)
+	for i in range(n):
+		index_noun = randrange(len(nouns))
+		index_verb = randrange(len(verbs))
+		index_adj = randrange(len(adj))
+		final_list.append(nouns.pop(index_noun))
+		final_list.append(verbs.pop(index_verb))
+		final_list.append(adj.pop(index_adj))
+
+	for token in sorted(final_list, key=lambda x: x.split("|")[1].strip()):
+		out.write(token)
+
+if __name__ == '__main__':
+	# Pfad zum baselist Lexikon als commandline argument
+	create_annotation_testset(sys.argv[1], 50)
