caa3fff960ae5fe83c20317e973a7d0d0fb0a9e4
Duke <dueker@cl.uni-heidelberg.de>
2018-06-04 15:10:36 +0200

    Nicht erkannte Wörter in Baselist handannotiert und anschließend noch einmal Duplikate entfernt

--- daten/Baselist/lemmatizer/remove_duplicate.py
+++ daten/Baselist/lemmatizer/remove_duplicate.py
@@ -1,7 +1,7 @@
 
 
 output = 'output/'
-path = 'output/Baselist.txt'
+path = 'output/Baselist_without_duplicates_lemmatized.txt'
 
 with open(path, 'r') as read_file:
     data = read_file.read().splitlines()
@@ -14,11 +14,11 @@ for word in data:
     else:
         d[word] = 1
 
-with open(output+'duplicates.txt', 'w') as write_file:
+with open(output+'duplicates_v2.txt', 'w') as write_file:
     for key in d:
         if(d[key] != 1):
             write_file.write(key + '\t' + str(d[key]) + '\n')
 
-with open(output+'Baselist_without_duplicates.txt', 'w') as write_file:
+with open(output+'Baselist_final_v1.txt', 'w') as write_file:
     for key in d:
         write_file.write(key+'\n')
