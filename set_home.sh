5fe7ed63d3a57f7180619af3039eac0405b0e4f8
dueker <dueker@ella.cl.uni-heidelberg.de>
2018-07-20 19:06:03 +0200

    bash-Skript für multiple ausführung hinzugefügt

---
+++ set_home.sh
@@ -0,0 +1,19 @@
+export JUNTO_DIR=~/Documents/junto
+export PATH=$PATH:$JUNTO_DIR/bin
+echo 'set variables done'
+date
+python3 run_junto_diff_config.py -R ~/Downloads/extended_mfw_200718/extended_mfw_200718_070 -mu2 0.01 -mu3 0.01
+date
+python3 run_junto_diff_config.py -R ~/Downloads/extended_mfw_200718/extended_mfw_200718_080_020 -mu2 0.01 -mu3 0.01
+date
+python3 run_junto_diff_config.py -R ~/Downloads/extended_mfw_200718/extended_mfw_200718_080_030 -mu2 0.01 -mu3 0.01
+date
+python3 run_junto_diff_config.py -R ~/Downloads/extended_mfw_200718/extended_mfw_200718_080 -mu2 0.01 -mu3 0.01
+date
+python3 run_junto_diff_config.py -R ~/Downloads/extended_mfw_200718/extended_mfw_200718_085_010 -mu2 0.01 -mu3 0.01
+date
+python3 run_junto_diff_config.py -R ~/Downloads/extended_mfw_200718/extended_mfw_200718_090_010 -mu2 0.01 -mu3 0.01
+date
+python3 run_junto_diff_config.py -R ~/Downloads/extended_mfw_200718/extended_mfw_200718_075_030 -mu2 0.01 -mu3 0.01
+date
+
