5b4d3dabd90aafb1ed79d8a707ff94835c21b9d7
Ulf2016 <steinbach@cl.uni-heidelberg.de>
2018-07-26 11:29:21 +0200

    baseline_classifier.py (demo version)

--- baseline_classifier.py
+++ baseline_classifier.py
@@ -18,6 +18,7 @@ import sys
 import string
 import json
 import math
+from pathlib import Path
 from operator import itemgetter
 from random import randrange
 from random import seed
@@ -38,7 +39,7 @@ from iwnlp.iwnlp_wrapper import IWNLPWrapper
 
 class Baseline_SVM:
 
-    def __init__(self, filename):
+    def __init__(self, filename, csv_filename):
         '''
 		Constructor of Baseline Class
 
@@ -46,12 +47,11 @@ class Baseline_SVM:
 		filename (str): path to the corpus file
         '''
         self.this_file = filename
+        self.csv_file = csv_filename
         
 
     def preprocess(self):
-        """
-        Preprocessing based on Scheffler et. al. German Twitter Preprocessing
-        """
+        
         tokenizedTweets_writer = open('./daten/tokenized_tweets_normalized.txt', 'w')
         preprocTweets_writer = open('./daten/preprocessed_tweets_normalized.txt', 'w')
         
@@ -99,6 +99,25 @@ class Baseline_SVM:
                 labels.append(l[1].strip())
         assert(len(tweets) == len(labels))
         return tweets, labels
+
+        # tweets = []
+        # labels = []
+        # with open('./daten/normalized_tweets_final.txt', 'r') as inputfile:
+            
+        #     for line in inputfile:
+        #         tweet = ""
+        #         l = line.split("\t")
+        #         tweets.append(l[0].strip())
+        #         labels.append(l[1].strip())
+        #         tweet_raw = l[0].strip()
+        #         for t in tweet_raw.split(" "):
+        #             w = t.split("/")[0].strip().lower()
+        #             if w not in stopwords.words('german'): 
+        #                 tweet += " " + w 
+        #         tweets.append(tweet.strip())
+        #         labels.append(l[1].strip())
+        # assert(len(tweets) == len(labels))
+        # return tweets, labels
 
     def get_n_folds(self, X, Y, n=10):
         """
@@ -179,7 +198,7 @@ class Baseline_SVM:
             Y_train_enc = le.fit_transform(Y_train)
             Y_test_enc = le.fit_transform(Y_test)
         
-            clf = svm.SVC(kernel='linear', C=0.5,random_state=1).fit(X_train_tfidf, Y_train_enc)
+            clf = svm.SVC(kernel='linear', C=1.0,random_state=1).fit(X_train_tfidf, Y_train_enc)
             
             predicted = clf.predict(X_test_tfidf)
             # scores.append(clf.score(X_test_tfidf, Y_test_enc))
@@ -193,21 +212,32 @@ class Baseline_SVM:
         self.write_score_to_csv(scores)
         print("Total Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
     
-    def baseline_mi(self, n=1500):
+    def baseline_mi(self, path, n=2000):
         vocab = []
         counter = 0
         with open('daten/mi_vocab.txt', 'r', encoding='utf-8') as input_file:
             for line in input_file:
                 if counter < n:
-                    token = line.split("\t")[0].strip()
+                    token = line.split("\t")[0].strip().lower()
                     vocab.append(token.split("/")[0].strip())  
                     counter += 1
 
+        print(len(vocab))
+
+        with open(path, "r") as f:
+            for line in f:
+                sw = line.split()[0].strip()
+                if sw not in stopwords.words('german'):
+                    vocab.append(line.split()[0].strip())
+
+        print(len(vocab))
+
         tweets, labels = self.read_corpus()
         ten_folds = self.get_n_folds(tweets, labels)
-        print("Baseline: mutual information feature selection")
-        self.write_label_to_csv("Baseline: feature selection w. mutual information")
-        bow_transformer = CountVectorizer(analyzer="word", min_df=2, lowercase=False, vocabulary=set(vocab))
+        print(str(path))
+        self.write_label_to_csv("Baseline: feature selection w. mutual information + extended lexicon")
+        self.write_label_to_csv(str(path))
+        bow_transformer = CountVectorizer(analyzer="word", min_df=1, lowercase=True, vocabulary=set(vocab))
         document_term_matrix = bow_transformer.fit(tweets)
         # print(bow_transformer.vocabulary_)
     
@@ -231,14 +261,153 @@ class Baseline_SVM:
             Y_train_enc = le.fit_transform(Y_train)
             Y_test_enc = le.fit_transform(Y_test)
         
-            clf = svm.SVC(kernel='linear', C=0.5,random_state=1).fit(document_term_matrix_tr, Y_train_enc)
+            clf = svm.SVC(kernel='linear', C=1.5, random_state=1).fit(document_term_matrix_tr, Y_train_enc)
             
             predicted = clf.predict(document_term_matrix_te)
             score = metrics.f1_score(Y_test_enc, predicted, average='weighted')
             scores.append(score)
             self.write_output_to_csv(i, X_test, Y_test, X_train, Y_train, score)
             print("Cross Validation #{0} --> avg. weighted F1-Score: {1}".format(i+1, score))
-        self.write_score_to_csv(scores)
+        self.write_score_to_csv(scores, "Total accuracy")
+
+    def check_token(self, t, lex, label):
+        if t.split("/")[0].strip().lower() not in stopwords.words('german') and (t.split("/")[0].strip().lower()) in set(lex):
+            if(label == "OFFENSE"):
+                print("tp because of " + t)
+                return 1        
+            elif label == "OTHER":
+                print("fp because of " + t)
+                return 2
+        else:
+            return 0
+            
+
+    def feature_extended_lexicon(self, path):
+        tweets, labels = self.read_corpus()
+        
+        extended_lexicon = []
+        print(len(extended_lexicon))
+        with open(path, "r") as f:
+            for line in f:
+                sw = line.split()[0].strip()
+                if sw not in stopwords.words('german'):
+                    extended_lexicon.append(line.split()[0].strip().lower())
+        print(str(path))
+        print(len(extended_lexicon))
+        print(len(set(extended_lexicon)))
+        tn = 0
+        fn = 0
+        tp = 0
+        fp = 0
+        dic = {}
+        with open('./daten/normalized_tweets_final.txt', 'r', encoding='utf-8') as preprocessed_tweets:
+            counter = 1
+            cc = 0
+            for line in preprocessed_tweets:
+                
+                print("tweet " + str(counter))
+                tweet = line.split("\t")[0].strip()
+                label = line.split("\t")[1].strip()
+                if label == "OFFENSE":
+                    cc += 1
+
+                try:
+                    counter += 1
+                    tokenlist = tweet.split(" ")
+                    # total_tokens += len(tokenlist)
+                    # count how many documents contain the token (avoid duplicate counts)
+                    for token in (tokenlist):
+                        r = self.check_token(token, extended_lexicon, label)
+                        if r == 1:
+                            tp += 1
+                            break
+                        elif r == 2:
+                            fp += 1
+                            break                    
+                                
+                                    
+                except IndexError as e:
+                    print("could not process token " + token)
+                    counter += 1
+        
+        print("TP: " + str(tp))
+        print("FP: " + str(fp))
+        fn = 1688 - tp
+        tn = 3320 - fp
+        print("FN: " + str(fn))
+        print("TN: " + str(tn))
+
+        precision = tp/(tp+fp)
+        recall = tp/(tp+fn)
+        f1 = 2*((precision*recall)/(precision+recall))
+        accuracy = (tp+tn)/(tp+tn+fp+fn)
+        print("precision: " + str(precision))
+        print("recall: " + str(recall))
+        print("f1: " + str(f1))
+        print("accuracy: " + str(accuracy))
+        # for k in sorted(dic, key=lambda k: len(dic[k]), reverse=True):
+        #     if len(dic[k]) > 1:
+        #         print(k, len(dic[k]))
+
+        # with open('./daten/extended_classification_output.txt', 'a') as f:
+        #     f.write(str(path) + "\n")
+        #     f.write("----------"+ "\n")
+        #     f.write("|" + str(tp) + "|" + str(fn) + "|" + "\n")
+        #     f.write("----------"+ "\n")
+        #     f.write("|" + str(fp) + "|" + str(tn) + "|" + "\n")
+        #     f.write("----------"+ "\n")
+        #     f.write("Precision: " + str(precision) + "\n")
+        #     f.write("Recall: " + str(recall) + "\n")
+        #     f.write("F1: " + str(f1) + "\n")
+        #     f.write("Accuracy: " + str(accuracy) + "\n")
+        #     f.write("#######################################")
+
+        # ten_folds = self.get_n_folds(tweets, labels)
+        # print("Extended lexicon: feature selection")
+        # self.write_label_to_csv(str(path))
+
+        # bow_transformer = CountVectorizer(analyzer="word", lowercase=True, vocabulary=set(extended_lexicon))
+        # document_term_matrix = bow_transformer.fit(tweets)
+        # scores = []
+        # scores2 = []
+        # scores3 = []
+
+        # for i in range(len(ten_folds)):
+            
+        #     test_fold = ten_folds[i]
+        #     train_folds = [fold for x, fold in enumerate(ten_folds) if x != i]
+        #     X_test, Y_test = test_fold[0], test_fold[1]
+        #     X_train, Y_train = [], []
+        #     for fold in range(len(train_folds)):
+        #         X_train.extend(train_folds[fold][0])
+        #         Y_train.extend(train_folds[fold][1])
+
+
+        #     assert(len(X_test) == len(Y_test))
+        #     assert(len(X_train) == len(Y_train))
+        #     document_term_matrix_tr = bow_transformer.fit_transform(X_train)
+        #     document_term_matrix_te = bow_transformer.fit_transform(X_test)
+
+        #     le = preprocessing.LabelEncoder()
+        #     Y_train_enc = le.fit_transform(Y_train)
+        #     Y_test_enc = le.fit_transform(Y_test)
+        
+        #     clf = svm.SVC(kernel='linear', C=1.0,random_state=1).fit(document_term_matrix_tr, Y_train_enc)
+            
+        #     predicted = clf.predict(document_term_matrix_te)
+        #     score = metrics.f1_score(Y_test_enc, predicted, average='weighted')
+        #     score2 = metrics.recall_score(Y_test_enc, predicted, average="weighted")
+        #     score3 = metrics.precision_score(Y_test_enc, predicted, average="weighted")
+        #     scores.append(score)
+        #     scores2.append(score2)
+        #     scores3.append(score3)
+        #     self.write_output_to_csv(i, X_test, Y_test, X_train, Y_train, score)
+        #     print("Cross Validation #{0} --> avg. weighted F1-Score: {1}".format(i+1, score))
+        #     # print("Recall --> {0}".format(score2))
+        #     # print("Precision --> {0}".format(score3))
+        # self.write_score_to_csv(scores, "Total accuracy")
+        # self.write_score_to_csv(scores2, "Avg. Recall")
+        # self.write_score_to_csv(scores3, "Avg. Precision")
 
 
     def feature_selector(self, tweets=[], labels=[]):
@@ -337,8 +506,7 @@ class Baseline_SVM:
             if y_test[i] == 'OFFENSE':
                 test_label_pos += 1
             else:
-                test_label_neg += 1
-            
+                test_label_neg += 1     
 
         for i, tweet in enumerate(x_train):
             train_instances += 1
@@ -348,30 +516,36 @@ class Baseline_SVM:
             else:
                 train_label_neg += 1
             
-
         # write to csv-file
-        with open('baseline_results.csv', 'a', encoding='utf-8') as output_csv:
+        with open(self.csv_file, 'a', encoding='utf-8') as output_csv:
             csv_writer = csv.writer(output_csv, delimiter=';',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
             csv_writer.writerow([foldNo+1, train_instances, train_label_pos, train_label_neg, round((train_tokens/train_instances),2), test_instances, test_label_pos, test_label_neg, float(test_tokens/test_instances), score ])
 
     def write_label_to_csv(self, label):
-        with open('baseline_results.csv', 'a', encoding='utf-8') as output_csv:
+        with open(self.csv_file, 'a', encoding='utf-8') as output_csv:
             csv_writer = csv.writer(output_csv, delimiter=';',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
             csv_writer.writerow([label])
 
-    def write_score_to_csv(self, scores):
+    def write_score_to_csv(self, scores, m):
         scores = np.array(scores)
-        with open('baseline_results.csv', 'a', encoding='utf-8') as output_csv:
+        with open(self.csv_file, 'a', encoding='utf-8') as output_csv:
             csv_writer = csv.writer(output_csv, delimiter=';',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            csv_writer.writerow(["", "", "", "", "", "", "", "", "", "Total Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)])
+            csv_writer.writerow(["", "", "", "", "", "", "", "", "", m + " : %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2)])
+            
 
 if __name__ == '__main__':
-    c = Baseline_SVM('./daten/germeval/germeval2018.training.txt')
-    c.baseline_mi()
-    #c.preprocess()
-    c.createBaselineClassifier(False)
-    c.createBaselineClassifier(True)
+    c = Baseline_SVM('./daten/germeval/germeval2018.training.txt', "extended_lexicon_results_testfinal.csv")
+    root_folder = Path("/Users/ulisteinbach/Desktop/SS18/software_projekt/softwareprojekt/daten/results/extracted_words_with_baselist/")
+    subfolders = [x for x in root_folder.iterdir() if not x.is_dir()]
+    for p in subfolders:
+        if ("extracted_words_with_baselist/extended_mfw_200718_075most_frequent_words_seeds_80_100" in (str(p)) or "extracted_words_with_baselist/extended_mfw_200718_080_020most_frequent_words_seeds_100_80" in str(p)):
+            c.baseline_mi(p)
+    # c.baseline_mi(p)
+    # c.baseline_mi()
+    # c.preprocess()
+    # c.createBaselineClassifier(False)
+    # c.createBaselineClassifier(True)
     # c.feature_selector()
